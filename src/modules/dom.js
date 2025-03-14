313887067aed607c008d451b83e1416a5475a4ad
2025314242 <donguk.kwon@yonsei.ac.kr>
2025-03-14 10:31:25 +0900

    feat: render
    page render

---
+++ src/modules/dom.js
@@ -0,0 +1,40 @@
+import { CONSTANTS } from './constants.js';
+import { loadTodos } from './storage.js';
+import { removeTodo, toggleTodo } from './todo.js';
+
+export function renderTodoList(date) {
+  const todoListContainer = document.getElementById('todo-list');
+  todoListContainer.innerHTML = '';
+
+  const todos = loadTodos(date);
+  todos.forEach((todo, index) => {
+    const li = document.createElement('li');
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = todo.checked;
+    checkbox.addEventListener('change', () => {
+      toggleTodo(date, index);
+      renderTodoList(date);
+    });
+
+    const textSpan = document.createElement('span');
+    textSpan.textContent = todo.text;
+    if (todo.checked) {
+      textSpan.style.textDecoration = 'line-through';
+    }
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = CONSTANTS.DELETE;
+    deleteBtn.addEventListener('click', () => {
+      removeTodo(date, index);
+      renderTodoList(date);
+    });
+
+    li.appendChild(checkbox);
+    li.appendChild(textSpan);
+    li.appendChild(deleteBtn);
+
+    todoListContainer.appendChild(li);
+  });
+}
